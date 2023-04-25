26951b23956c337abd6ad3cd90247b81f7c070a6
Kajal Mishra <kmishra.slg@gmail.com>
2023-04-25 14:58:02 +0530

    Updated Callable Statement

--- src/Application.java
+++ src/Application.java
@@ -7,24 +7,38 @@ public class Application {
         // 1. Register and load the Driver
         Class.forName("com.mysql.cj.jdbc.Driver");
 
+
         // 2. Build the connection with DriverManager.getConnection
         Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/employee",
                 "root",
                 "12345");
 
-        // 3. Create a statement
-        Statement statement = connection.createStatement();
+
+        // 3. c) Call the stored procedures - CALLABLE STATEMENT
+        // I -- get_num_of_empl
+        CallableStatement callableStatement = connection.prepareCall("{call get_num_of_empl(?)}");
+        callableStatement.registerOutParameter(1, Types.INTEGER); // OUT PARAMETER
+
+        // II -- get_first_name
+        CallableStatement callableStatement1 = connection.prepareCall("{call get_first_name(?,?)}");
+        callableStatement1.setInt(1, 1001); // IN PARAMETER
+        callableStatement1.registerOutParameter(2, Types.VARCHAR); // OUT PARAMETER
+
 
         // 4. Execute the query and fetch the result set
-        ResultSet resultSet = statement.executeQuery("select * from employeesalary");
+        callableStatement.execute();
+        callableStatement1.execute();
+
 
         // 5. Traverse through the resultset
-        while(resultSet.next()){
-            System.out.println(resultSet.getInt(1)+" "
-                    + resultSet.getString(2)+" "
-                    + resultSet.getInt(3));
-        }
+        System.out.println(callableStatement.getInt(1));
+        System.out.println(callableStatement1.getString(2));
+
+        callableStatement.close();
+        callableStatement1.close();
+
+
         // 6. Close the connection
         connection.close();
     }
