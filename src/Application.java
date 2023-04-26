2d07035e6c86a4e781c9344cdeeee4ec5c2db095
Kajal Mishra <kmishra.slg@gmail.com>
2023-04-26 23:24:04 +0530

    Added Code to Connection Pooling

--- src/Application.java
+++ src/Application.java
@@ -1,31 +1,60 @@
 import java.sql.*;
-
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+import javax.sql.PooledConnection;
 public class Application {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        // 1. Register and load the Driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        private static final String DRIVER="com.mysql.cj.jdbc.Driver";
+        private static final String URL="jdbc:mysql://localhost:3306";
+        private static final String USER_NAME="root";
+        private static final String PASSWORD="12345";
+        private static final String INSERT_SQL =    "INSERT INTO Employee.EmployeeDemographics " +
+                                                    "(EmployeeID, FirstName, LastName, Age, Gender)" +
+                                                    " VALUES (2004, 'Mary', 'Kom', '35', 'Female')";
+        private static final String UPDATE_SQL =    "UPDATE Employee.EmployeeDemographics " +
+                                                    "SET Age = 31 " +
+                                                    "WHERE EmployeeID = 2001";
+        private static PooledConnection pooledConnection = null;
+        private static Connection proxyCon1 = null;
+        private static Connection proxyCon2 = null;
 
-        // 2. Build the connection with DriverManager.getConnection
-        Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/employee",
-                "root",
-                "12345");
+        public static void main(String[] args) throws ClassNotFoundException {
 
-        // 3. Create a statement
-        Statement statement = connection.createStatement();
+            Class.forName(DRIVER);
 
-        // 4. Execute the query and fetch the result set
-        ResultSet resultSet = statement.executeQuery("select * from employeesalary");
+            // Connection - Pooling
+            MysqlConnectionPoolDataSource mysqlConnectionPoolDataSource = new MysqlConnectionPoolDataSource();
+            mysqlConnectionPoolDataSource.setURL(URL);
+            mysqlConnectionPoolDataSource.setUser(USER_NAME);
+            mysqlConnectionPoolDataSource.setPassword(PASSWORD);
 
-        // 5. Traverse through the resultset
-        while(resultSet.next()){
-            System.out.println(resultSet.getInt(1)+" "
-                    + resultSet.getString(2)+" "
-                    + resultSet.getInt(3));
-        }
-        // 6. Close the connection
-        connection.close();
+            try{
+                PooledConnection pooledConnection = mysqlConnectionPoolDataSource.getPooledConnection();
+                System.out.println("PooledConnection is created");
+
+                // Creating connection - proxyCon1
+                proxyCon1 = pooledConnection.getConnection();
+                Statement statement = proxyCon1.createStatement();
+                statement.execute(INSERT_SQL);
+                statement.close();
+
+                // Creating connection - proxyCon2
+                proxyCon2 = pooledConnection.getConnection();
+                Statement statement1 = proxyCon2.createStatement();
+                statement1.execute(UPDATE_SQL);
+                statement1.close();
+
+            } catch (SQLException ex){
+                throw new RuntimeException(ex);
+            } finally {
+                try{
+                    if(proxyCon1 != null)   proxyCon1.close();
+                    if(proxyCon2 != null)   proxyCon1.close();
+                    if(pooledConnection != null) pooledConnection.close();
+                }catch (SQLException ex){
+                    throw new RuntimeException(ex);
+                }
+            }
     }
 }
